d448585b15aa4f7b9cf7c32986d867b579a37fcd
Giedrius <giedrius@blokas.io>
2026-05-07 10:12:19 +0300

    Enable QProcess on macOS

--- configure.blokas.mac.sh
+++ configure.blokas.mac.sh
@@ -8,7 +8,7 @@ ${QTDIR}/configure -prefix ${PREFIX} -debug-and-release -platform macx-clang -op
 	-skip qtgamepad -skip qtgraphicaleffects -skip qtlocation -skip qtmultimedia -skip qtpurchasing -skip qtquickcontrols -skip qtquickcontrols2 \
 	-skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus -skip qtserialport -skip qttranslations -skip qtvirtualkeyboard -skip qtwayland \
 	-skip qtwebchannel -skip qtwebengine -skip qtwebsockets -skip qtwebview -skip qtxmlpatterns -no-feature-textodfwriter -no-feature-sharedmemory \
-	-no-feature-systemsemaphore -no-feature-im -no-feature-process -no-feature-dom -no-feature-printpreviewwidget -no-feature-printpreviewdialog \
+	-no-feature-systemsemaphore -no-feature-im -no-feature-dom -no-feature-printpreviewwidget -no-feature-printpreviewdialog \
 	-no-feature-cups -no-feature-fontdialog -no-feature-datawidgetmapper \
 	-no-feature-colornames -no-feature-cups -no-feature-paint_debug \
 	-no-feature-codecs -no-feature-big_codecs -no-feature-iconv -no-feature-ftp -no-feature-udpsocket -no-feature-networkproxy -no-feature-socks5 -no-feature-networkdiskcache \
